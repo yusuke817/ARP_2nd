8929966c87d78e5a2f21fb3bdb7e529dc4957adc
kidoyusuke817 <usk817@keio.jp>
2021-12-28 15:11:20 +0000

    1228

--- named.c
+++ named.c
@@ -15,79 +15,79 @@
 
 float seconds = 0.0;
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
     int fd_named;
     time_t start;
     time_t end;
-    
+
     mkfifo("/tmp/named", 0666);
 
     printf("Please input the number of elements of the array\n");
 
     int num;
 
-    scanf("%d", & num);
+    scanf("%d", &num);
 
-    if (num > MAX_SIZE){
+    if (num > MAX_SIZE)
+    {
 
         printf("You can input less than 1250000\n");
-
-
     }
 
     int id = fork();
 
-    if (id == -1){
+    if (id == -1)
+    {
 
         printf("Error \n");
         exit(1);
     }
 
-    if (id == 0){
+    start = clock();
+
+    if (id == 0)
+    {
 
         printf("Start");
 
         int P[num];
 
-        for(int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++)
+        {
 
-            P[i] = 1 + rand()%1000;
-
+            P[i] = 1 + rand() % 1000;
         }
 
         fd_named = open("/tmp/named", O_WRONLY);
 
         // Stores time seconds
 
-        start = clock();
-
-        for(int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++)
+        {
 
             write(fd_named, &P[i], sizeof(P[i]));
         }
-        
-        
     }
 
     //printf("End");
-        
+
     fd_named = open("/tmp/named", O_RDONLY);
 
     int C[num];
-	
-                
-    for(int i = 0; i < num; i++){
 
-    	read(fd_named, &C[i], sizeof(C[i]));
+    for (int i = 0; i < num; i++)
+    {
 
+        read(fd_named, &C[i], sizeof(C[i]));
     }
 
     end = clock();
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-    printf("Time of execution : %f\n", seconds); 
-       
-    close(fd_named);    
+    printf("Time of execution : %f\n", seconds);
+
+    close(fd_named);
     //wait(NULL);
 
     return 0;
